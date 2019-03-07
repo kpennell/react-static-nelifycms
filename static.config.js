64c42ca9cb64fe705ea1291f23f8c57da83c954b
Kyle Pennell <kpennell@gmail.com>
2019-03-07 16:42:22 -0600

    heyweffewfo

--- static.config.js
+++ static.config.js
@@ -9,31 +9,31 @@ import { string } from './src/constants';
 
 import { theme } from './src/theme';
 
-// export default {
-//   getSiteData: () => ({
-//     title: 'React Static',
-//   }),
-//   getRoutes: async () => {
-//     const { data: posts } = await axios.get(
-//       'https://jsonplaceholder.typicode.com/posts'
-//     )
-//     return [
-//       {
-//         path: '/blog',
-//         getData: () => ({
-//           posts,
-//         }),
-//         children: posts.map(post => ({
-//           path: `/post/${post.id}`,
-//           component: 'src/containers/Post',
-//           getData: () => ({
-//             post,
-//           }),
-//         })),
-//       },
-//     ]
-//   },
-// }
+export default {
+  getSiteData: () => ({
+    title: 'React Static',
+  }),
+  getRoutes: async () => {
+    const { data: posts } = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    )
+    return [
+      {
+        path: '/blog',
+        getData: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          component: 'src/containers/Post',
+          getData: () => ({
+            post,
+          }),
+        })),
+      },
+    ]
+  },
+}
 
 
 export default {
